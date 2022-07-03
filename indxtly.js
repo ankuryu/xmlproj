43df126dabe59cadcfe84ba34828eecc01baad12
ankuryu <pmco89@gmail.com>
2022-07-03 22:29:51 +0530

    Makes  csv from the xml

--- indxtly.js
+++ indxtly.js
@@ -3,6 +3,7 @@ var fs = require('fs'),
     xml2js = require('xml2js');
 
 var parser = new xml2js.Parser();
+let lgr = fs.createWriteStream('pmmas02Jul.csv')
 fs.readFile(__dirname + '/pmtlymas02Jul.xml', function(err, data) {
     parser.parseString(data, function (err, result) {
        // console.dir(result.ENVELOPE);
@@ -14,7 +15,7 @@ fs.readFile(__dirname + '/pmtlymas02Jul.xml', function(err, data) {
         console.log("Length :",ln)
         console.log('Done');
         //tlyobj = result.ENVELOPE.BODY[0].IMPORTDATA[0].REQUESTDATA[0].TALLYMESSAGE[n]
-        for(n=0;n<11;n++){
+        for(n=0;n<ln;n++){
           tlyobj = result.ENVELOPE.BODY[0].IMPORTDATA[0].REQUESTDATA[0].TALLYMESSAGE[n]
           //console.dir(tlyobj)
           console.dir('========================')
@@ -22,9 +23,25 @@ fs.readFile(__dirname + '/pmtlymas02Jul.xml', function(err, data) {
           tr = Object.entries(tlyobj)
           console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>')
           av = tr[1][1]
+          ki = tr[1][0]   // get the key alias type
           console.dir(tr[1][0])
+          // now get the parent
+          if(ki == 'LEDGER' || ki=='GROUP'){
+
+            prnt = av[0].PARENT[0]
+            //console.log( '++',av[0].PARENT )
+            lnglst =av[0]['LANGUAGENAME.LIST'][0]
+            nmlst = lnglst['NAME.LIST'][0].NAME
+            console.log(nmlst.length)
+            console.log('>>>',ki,prnt,nmlst[0])
+            lgr.write(ki+","+prnt+","+nmlst[0]+"\n")
+
+          }
+
+          // get all the names
           console.dir(av[0]['$'].NAME)
         }
+        lgr.end()
         console.dir(result.ENVELOPE.BODY[0].IMPORTDATA[0].REQUESTDATA[0].TALLYMESSAGE[0])
 //        console.dir(JSON.stringify(result,null,' '))
    //   console.dir(result.root.TALLYMESSAGE[0].VOUCHER[0].DATE)
